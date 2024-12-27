88dda9aca66e76e41365617a5f5c96c5ccbf3bc8
khakomaki <hakomaki.karri@gmail.com>
2024-12-28 00:32:21 +0200

    switched to case insensitive ordering

--- app/src/main/java/com/example/drinkify/core/database/DrinkDao.kt
+++ app/src/main/java/com/example/drinkify/core/database/DrinkDao.kt
@@ -12,12 +12,13 @@ interface DrinkDao {
     @Upsert
     suspend fun upsertDrink(drink: Drink)
 
-    @Query("SELECT * FROM drink_table ORDER BY name ASC")
+    // case insensitive order
+    @Query("SELECT * FROM drink_table ORDER BY name COLLATE NOCASE ASC")
     fun getAllDrinksByName(): Flow<List<Drink>>
 
-    @Query("SELECT * FROM drink_table ORDER BY amountInMl ASC")
+    @Query("SELECT * FROM drink_table ORDER BY amountInMl DESC")
     fun getAllDrinksByAmount(): Flow<List<Drink>>
 
-    @Query("SELECT * FROM drink_table ORDER BY alcoholPercentage ASC")
+    @Query("SELECT * FROM drink_table ORDER BY alcoholPercentage DESC")
     fun getAllDrinksByAlcoholPercentage(): Flow<List<Drink>>
 }
