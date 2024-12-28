6292135139f566746bd969b0aa4d5c84929520d5
khakomaki <hakomaki.karri@gmail.com>
2024-12-28 23:43:09 +0200

    removed unused imports and variables

--- app/src/main/java/com/example/drinkify/MainActivity.kt
+++ app/src/main/java/com/example/drinkify/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.example.drinkify.core.database.DrinkDatabase
-import com.example.drinkify.drinks.DrinkScreen
 import com.example.drinkify.drinks.DrinkViewModel
 import com.example.drinkify.navi.AppNavigation
 
@@ -34,8 +31,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val state by viewModel.state.collectAsState()
-            // DrinkScreen(state = state, onEvent = viewModel::onEvent)
             AppNavigation(viewModel = viewModel)
         }
     }
--- app/src/main/java/com/example/drinkify/drinks/DrinkScreen.kt
+++ app/src/main/java/com/example/drinkify/drinks/DrinkScreen.kt
@@ -15,14 +15,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
