95c08a09f56fde6d10c608de4d9a35be0f80e673
khakomaki <hakomaki.karri@gmail.com>
2025-01-07 03:30:14 +0200

    localization for BAC, improved UI

--- app/src/main/java/com/example/drinkify/mainscreen/MainScreen.kt
+++ app/src/main/java/com/example/drinkify/mainscreen/MainScreen.kt
@@ -1,10 +1,13 @@
 package com.example.drinkify.mainscreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,26 +25,45 @@ fun MainScreen(
     onNavigateToDrinkHistory: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxWidth().padding(16.dp)
-    ) {
-        // BAC meter
-        BACMeter(bac = bacState.bac)
-
-        // Navigation
-        Button(onClick = onNavigateToDrinks) {
-            Text("Drinks")
-        }
-        Button(onClick = onNavigateToProfile) {
-            Text("Profile")
-        }
-        Button(onClick = onNavigateToRecordDrink) {
-            Text("Record Drinks")
-        }
-        Button(onClick = onNavigateToDrinkHistory) {
-            Text("Drink History")
+    Scaffold(
+        // BAC Meter
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier.padding(bottom = 32.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                        //.padding(vertical = 16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    BACMeter(bac = bacState.bac)
+                }
+            }
+        },
+        modifier = modifier
+    ) { padding ->
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            // Navigation
+            Button(onClick = onNavigateToDrinks) {
+                Text("Drinks")
+            }
+            Button(onClick = onNavigateToProfile) {
+                Text("Profile")
+            }
+            Button(onClick = onNavigateToRecordDrink) {
+                Text("Record Drinks")
+            }
+            Button(onClick = onNavigateToDrinkHistory) {
+                Text("Drink History")
+            }
         }
     }
 }
--- app/src/main/java/com/example/drinkify/ui/components/BACMeter.kt
+++ app/src/main/java/com/example/drinkify/ui/components/BACMeter.kt
@@ -3,11 +3,23 @@ package com.example.drinkify.ui.components
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import java.util.Locale
 
 @Composable
 fun BACMeter(bac: Double) {
+    // use promille / per mille for certain countries
+    val currentLocale = Locale.getDefault()
+    val isPerMille = currentLocale.country in listOf("FI", "GE", "NL", "SE")
+    val formattedBAC = if (isPerMille) {
+        String.format(currentLocale,"%.2f", bac * 10.0) + "‰"
+    } else {
+        String.format(currentLocale,"%.3f", bac) + "%"
+    }
+
     Text(
-        text = "BAC: ${"%.3f".format(bac)}",
-        style = MaterialTheme.typography.headlineLarge
+        text = "BAC: $formattedBAC",
+        style = MaterialTheme.typography.headlineLarge,
+        fontWeight = FontWeight.Bold
     )
 }
